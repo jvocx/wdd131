b2f0ebf3968d7df1777247534d482f9c653cb928
João Victor Oliveira Cavalcante <jvocx.dev@gmail.com>
2026-01-10 10:05:37 -0300

    ajuste

--- W01_Assignment/scripts/getdates.js
+++ W01_Assignment/scripts/getdates.js
@@ -1,6 +1,6 @@
-// Get current year
+
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
-// Get last modified date
+
 document.getElementById("lastModified").textContent = document.lastModified;
