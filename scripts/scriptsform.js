ac7acba1cfd764322df45b53475e83d1bf34da2e
João Victor Oliveira Cavalcante <jvocx.dev@gmail.com>
2026-02-14 08:53:30 -0300

    ajuste

--- scripts/scriptsform.js
+++ scripts/scriptsform.js
@@ -5,19 +5,20 @@ document.getElementById("currentYear").textContent =
 
 
 const products = [
-    { id: "p1", name: "Air Fryer XL" },
-    { id: "p2", name: "Smart TV 55\"" },
-    { id: "p3", name: "Bluetooth Speaker" },
-    { id: "p4", name: "Coffee Machine Pro" },
-    { id: "p5", name: "Wireless Headphones" }
+    { id: "fc-1888", name: "Flux Capacitor" },
+    { id: "fc-2050", name: "Power Laces" },
+    { id: "fs-1987", name: "Time Circuits" },
+    { id: "ac-2000", name: "Mr. Fusion" },
+    { id: "jj-1969", name: "Hoverboard" }
 ];
-
 
 const productSelect = document.getElementById("productName");
 
 products.forEach(product => {
     const option = document.createElement("option");
-    option.value = product.id;
+
+    option.value = product.id;   // 
     option.textContent = product.name;
+
     productSelect.appendChild(option);
 });
